69305ed9de15c91ccce36b4e5f927341a404728a
bhagya199019 <117078616+bhagya199019@users.noreply.github.com>
2024-06-27 19:37:15 +0530

    Update Application.java

--- src/main/java/com/web/Application.java
+++ src/main/java/com/web/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-	@ComponentScan(basePackages={"com.web.control
+	@ComponentScan(basePackages={"com.web.controller"})
 public class Application {
 
 	public static void main(String[] args) {
